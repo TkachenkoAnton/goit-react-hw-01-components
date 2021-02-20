defb38352a62d6e62acf28ee4e42e66d22f32030
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-02-20 20:25:07 +0200

    add new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import users from "./components/SocialProfile/user.json";
+import SocialProfile from "./components/SocialProfile/SocialProfile";
 
 const App = () => {
-  return <h1>hifkghgjhff</h1>;
+  return (
+    <div>
+      {users.map(({ name, tag, location, avatar, stats }) => (
+        <SocialProfile
+          name={name}
+          tag={tag}
+          location={location}
+          avatar={avatar}
+          stats={stats}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SocialProfile/SocialProfile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SocialProfile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt="Аватар пользователя" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialProfile;
