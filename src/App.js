f1237695c46d2dafd377a1fdc177347cff081bf5
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-02-21 12:11:07 +0200

    add FriendList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import users from "./components/SocialProfile/user.json";
 import statisticalData from "./components/Statistics/statistical-data.json";
+import friends from "./components/FriendList/friends.json";
 import SocialProfile from "./components/SocialProfile/SocialProfile";
 import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
 
 const App = () => {
   return (
     <>
       <SocialProfile usersProfiles={users} />
       <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
     </>
   );
 };
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id}>
+          <span>{isOnline ? "Online" : "Offline"}</span>
+          <img src={avatar} alt={name} width="48" />
+          <p>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
