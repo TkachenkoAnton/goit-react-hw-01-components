c0bf0d69ee940803b7e26c9d65c43c04b4c53af2
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-02-21 16:08:20 +0200

    add styles for Statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,10 +4,10 @@ import styles from "./statistics.module.scss";
 const Statistics = ({ title, stats }) => {
   return (
     <section className={styles.card}>
-      {title && <h2>{title}</h2>}
-      <ul>
+      {title && <h2 className={styles.statisticTitle}>{title}</h2>}
+      <ul className={styles.statisticList}>
         {stats.map(({ id, label, percentage }) => (
-          <li key={id}>
+          <li key={id} className={styles.statisticItems}>
             <span>{label}</span>
             <span>{percentage}%</span>
           </li>
